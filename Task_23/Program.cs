2b4b6f8bc236482e6aeb25278cc22a9b3c4a2eb5
Maxim <ilyukhin.m@internet.ru>
2022-11-23 22:15:09 +0300

    first commit

---
+++ Task_23/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+using static System.Console;
+Clear();
+
+Write("Enter N = ");
+int N = int.Parse(ReadLine());
+for(int i = 1; i<N; i++)
+    Write(Math.Pow(i,3) + ", ");
+Write(Math.Pow(N,3));    
